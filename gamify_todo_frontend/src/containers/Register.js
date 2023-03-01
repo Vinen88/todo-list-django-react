c7111094347c4b29691bf095a24f943809358752
Jason Robock <jrobock@ualberta.ca>
2023-03-01 10:43:51 -0700

    front end stuff is hard

---
+++ gamify_todo_frontend/src/containers/Register.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const register = () => (
+    <div>
+        Register    
+    </div>
+);
+
+export default register;
---
+++ gamify_todo_frontend/src/hocs/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import { Fragment } from 'react';
+
+const layout = ({ children }) => (
+     <Fragment>
+        <Navbar />
+        {children}
+     </Fragment>
+);
+
+export default layout;
